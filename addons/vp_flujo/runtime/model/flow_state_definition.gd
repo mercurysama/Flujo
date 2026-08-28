00c9f54e5c5956e0b52a4759bb33c12a7b0af3b8
mercurysama <mercurysama@gmail.com>
2026-08-28 01:08:38 -0600

    feat: añadir definición de estados de Flujo

---
+++ addons/vp_flujo/runtime/model/flow_state_definition.gd
@@ -0,0 +1,10 @@
+class_name FlowStateDefinition
+extends FlowBlockContainer
+
+@export var is_initial: bool = false
+
+func _init() -> void:
+	display_name = "State"
+
+func duplicate_state_with_new_ids() -> FlowStateDefinition:
+	return duplicate_with_new_ids() as FlowStateDefinition
